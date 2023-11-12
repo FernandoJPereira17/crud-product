c6412a5931a12f96a2b5e46be5274ebc845949df
FernandoJPereira <fernandojosepereira17@gmail.com>
2023-11-12 12:43:39 -0300

    Functions Edit e Delete Ok

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const tableData = document.getElementById("tableContent");
 const btnAddProduct = document.getElementById("addProduct");
 const modal = document.getElementById("modal");
+const modalSalvar = document.getElementById("modalSalvar");
 const productName = document.getElementById("productName");
 const productValue = document.getElementById("productValue");
 const productQuantity = document.getElementById("productQuantity");
@@ -8,6 +9,7 @@ const btnSend = document.getElementById("send");
 const btnCancel = document.getElementById("cancel");
 const form = document.getElementById("form");
 const errorMessage = document.getElementById("errorMessage");
+
 
 const products = [
   {
@@ -20,6 +22,11 @@ const products = [
     preco: 756.0,
     quantidade: 10,
   },
+  {
+    nome: "MacBook",
+    preco: 2000,
+    quantidade: 2,
+  },
 ];
 
 function formatDataToString(value) {
@@ -31,12 +38,72 @@ function formatDataToString(value) {
   return valorFormatado;
 }
 
-function handleDelete(id) {
-  alert(products[id]["nome"]);
+function handleEdit(id) {
+  openModal();
+  
+  const product = products.find((product, index) => index === id);
+    productName.value = product.nome;
+    productValue.value = product.preco;
+    productQuantity.value = product.quantidade;
+
+  const newName = product.nome;
+  const newPreco = product.preco;
+  const newQuantidade = product.quantidade;
+
+  products[id].nome = newName;
+  products[id].preco = newPreco;
+  products[id].quantidade = newQuantidade;
+
+  if (product !== products) {
+    products.splice(id, 1);
+    tableContent.innerHTML = products;
+    products.map((product, index) => {
+      return (tableContent.innerHTML += `
+        <tr class="even:bg-[#f2f2f2] odd:bg-white">
+          <td class="p-3">${product.nome}</td>
+          <td class="p-3">${product.preco}</td>
+          <td class="p-3">${product.quantidade}</td>
+          <td class="text-center p-3">${product.total}</td>
+          <td class="flex gap-2 items-center justify-center p-3">
+            <button onclick="handleEdit(${index})" class="py-1 px-2 bg-sky-700 text-white rounded-md">
+              Editar
+            </button>
+            <button onclick="handleDelete(${index})" class="py-1 px-2 bg-red-800 text-white rounded-md">
+              Deletar
+            </button>
+          </td>
+        </tr>
+    `);
+    });
+    
+  } else {
+    showErrorMessage();
+  }
+  return renderDataTable();
 }
 
-function handleEdit(id) {
-  alert(products[id]["nome"]);
+function handleDelete(id) {
+  products.splice(id, 1);
+  tableContent.innerHTML = "";
+  products.map((product, index) => {
+    return (tableContent.innerHTML += `
+    <tr class="even:bg-[#f2f2f2] odd:bg-white">
+    <td class="p-3">${product.nome}</td>
+    <td class="p-3">${product.preco}</td>
+    <td class="p-3">${product.quantidade}</td>
+    <td class="text-center p-3">${product.total}</td>
+    <td class="flex gap-2 items-center justify-center p-3">
+      <button onclick="handleEdit(${index})" class="py-1 px-2 bg-sky-700 text-white rounded-md">
+        Editar
+      </button>
+      <button onclick="handleDelete(${index})" class="py-1 px-2 bg-red-800 text-white rounded-md">
+        Deletar
+      </button>
+    </td>
+  </tr>
+    `);
+  });
+  renderDataTable();
 }
 
 function createTableBodyRow(nome, preco, quantidade, total, id) {
@@ -74,7 +141,6 @@ function renderDataTable() {
       totalFormatado,
       index
     );
-
     return data;
   });
 }
@@ -115,8 +181,8 @@ function addProduct() {
     showErrorMessage();
   }
 }
-
 renderDataTable();
+
 
 btnAddProduct.addEventListener("click", openModal);
 
@@ -130,3 +196,4 @@ btnCancel.addEventListener("click", (e) => {
   e.preventDefault();
   closeModal();
 });
+
